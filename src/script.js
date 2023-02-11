6883d9da405f1c4cf7da6279e23e20b682337340
Jérémy Brialon <jeremy.brialon@merkle.com>
2023-02-11 21:52:30 +0100

    camera adjustments

--- src/script.js
+++ src/script.js
@@ -25,7 +25,7 @@ let camera = new PerspectiveCamera(
   0.1,
   1000
 );
-camera.position.z = 1.4;
+camera.position.z = 0.8;
 
 let renderer = new WebGLRenderer({ antialias: true, transparent: true });
 renderer.setSize(window.innerWidth, window.innerHeight);
