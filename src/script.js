9d6ebde4ff2ce3d2e76c30983fc5fa1915b9485b
Jérémy Brialon <jeremy.brialon@merkle.com>
2023-02-14 14:30:18 +0100

    draw line with points

--- src/script.js
+++ src/script.js
@@ -1,25 +1,19 @@
-import {
-  Scene,
-  SphereGeometry,
-  Vector3,
-  PerspectiveCamera,
-  WebGLRenderer,
-  Color,
-  MeshBasicMaterial,
-  Mesh,
-  Clock,
-  AudioListener,
-  Audio,
-  AudioLoader,
-  AudioAnalyser,
-} from "three";
+import * as THREE from "three";
 import { OrbitControls } from "three/examples/jsm/controls/OrbitControls.js";
 import { createSculptureWithGeometry } from "https://unpkg.com/shader-park-core/dist/shader-park-core.esm.js";
 import { spCodeTexture, spCodeSphere } from "/sp-code.js";
 
-let scene = new Scene();
+import * as dat from "lil-gui";
 
-let camera = new PerspectiveCamera(
+/**
+ * Base
+ */
+// Debug
+const gui = new dat.GUI();
+
+let scene = new THREE.Scene();
+
+let camera = new THREE.PerspectiveCamera(
   75,
   window.innerWidth / window.innerHeight,
   0.1,
@@ -27,27 +21,27 @@ let camera = new PerspectiveCamera(
 );
 camera.position.z = 1.2;
 
-let renderer = new WebGLRenderer({ antialias: true, transparent: true });
+let renderer = new THREE.WebGLRenderer({ antialias: true, transparent: true });
 renderer.setSize(window.innerWidth, window.innerHeight);
 renderer.setPixelRatio(window.devicePixelRatio);
-renderer.setClearColor(new Color(1, 1, 1), 0);
+renderer.setClearColor(new THREE.Color(1, 1, 1), 0);
 document.body.appendChild(renderer.domElement);
 
-let clock = new Clock();
+let clock = new THREE.Clock();
 
 let button = document.querySelector(".button");
 button.innerHTML = "Loading Audio...";
 
 // create an AudioListener and add it to the camera
-const listener = new AudioListener();
+const listener = new THREE.AudioListener();
 camera.add(listener);
 
 // create an Audio source
-const sound = new Audio(listener);
+const sound = new THREE.Audio(listener);
 
 // load a sound and set it as the Audio object's buffer
-const audioLoader = new AudioLoader();
-audioLoader.load("/audio/4.flac", function (buffer) {
+const audioLoader = new THREE.AudioLoader();
+audioLoader.load("/audio/3.wav", function (buffer) {
   sound.setBuffer(buffer);
   sound.setLoop(true);
   sound.setVolume(0.5);
@@ -58,18 +52,16 @@ audioLoader.load("/audio/4.flac", function (buffer) {
     button.style.display = "none";
     sound.play();
   });
-  // sound.play();
 });
 
 // create an AudioAnalyser, passing in the sound and desired fftSize
-const analyser = new AudioAnalyser(sound, 32);
-
+const analyser = new THREE.AudioAnalyser(sound, 32);
+console.log(analyser);
 // get the average frequency of the sound
 const data = analyser.getAverageFrequency();
-
 let state = {
-  mouse: new Vector3(),
-  currMouse: new Vector3(),
+  mouse: new THREE.Vector3(),
+  currMouse: new THREE.Vector3(),
   pointerDown: 0.0,
   currPointerDown: 0.0,
   size: 0.5,
@@ -79,21 +71,116 @@ let state = {
 };
 
 // create our geometry and material
-let geometry = new SphereGeometry(2, 45, 45);
+//let geometry = new SphereGeometry(2, 45, 45);
 // let material = new MeshBasicMaterial({ color: 0x33aaee });
 // let mesh = new Mesh(geometry, material);
 
-let mesh = createSculptureWithGeometry(geometry, spCodeTexture(), () => {
-  return {
-    time: state.time,
-    pointerDown: state.pointerDown,
-    mouse: state.mouse,
-    audio: state.audio,
-    _scale: 0.5,
-  };
-});
+// let mesh = createSculptureWithGeometry(geometry, spCodeTexture(), () => {
+//   return {
+//     time: state.time,
+//     pointerDown: state.pointerDown,
+//     mouse: state.mouse,
+//     audio: state.audio,
+//     _scale: 0.5,
+//   };
+// });
 
-scene.add(mesh);
+// scene.add(mesh);
+
+/**
+ * Particles
+ */
+
+const parameters = {
+  count: 100000,
+  size: 0.01,
+  radius: 5,
+  branches: 3,
+  spin: 1,
+  randomness: 0.2,
+  randomnessPower: 3,
+  insideColor: "#ff7300",
+  outsideColor: "#ffffff",
+};
+
+let geometry = null;
+let material = null;
+let points = null;
+let indexPosition = 0;
+let positions = new Float32Array(3000 * 3 * 3);
+
+const generatePoints = () => {
+  if (points !== null) {
+    geometry.dispose();
+    material.dispose();
+    scene.remove(points);
+  }
+
+  geometry = new THREE.BufferGeometry();
+  // const positions = new Float32Array(parameters.count * 3 * 3);
+  const colorsArray = new Float32Array(parameters.count * 3 * 3);
+
+  // for (let index = 0; index < parameters.count * 3 * 3; index++) {
+  //   const i3 = index * 3;
+
+  //   positions[i3 + 0] = index / 100; // AXE X
+  //   positions[i3 + 1] = Math.sin(index / 100, 3); // AXE Y
+  //   positions[i3 + 2] = 1; // AYE Z
+  // }
+  const positionsAttribute = new THREE.BufferAttribute(positions, 3);
+
+  geometry.setAttribute("position", positionsAttribute);
+
+  material = new THREE.PointsMaterial({
+    size: 0.01,
+    sizeAttenuation: true,
+    transparent: true,
+    // color: '#ff88cc',
+    // alphaTest: 0.001,
+    // depthTest: false,
+    depthWrite: false,
+    blending: THREE.AdditiveBlending,
+    vertexColors: true,
+  });
+
+  // Points
+  points = new THREE.Points(geometry, material);
+  scene.add(points);
+};
+
+generatePoints();
+
+const addToPosition = (y) => {
+  const index = indexPosition;
+  const i3 = index * 3;
+
+  positions[i3 + 0] = index / 500; // AXE X
+  positions[i3 + 1] = y; // AXE Y
+  positions[i3 + 2] = 1; // AYE Z
+};
+
+const paramFolder = gui.addFolder("Curve generatePoints");
+paramFolder
+  .add(parameters, "count", 0, 1000000, 100)
+  .onFinishChange(generatePoints);
+paramFolder
+  .add(parameters, "size", 0.001, 0.1, 0.001)
+  .onFinishChange(generatePoints);
+paramFolder
+  .add(parameters, "radius", 0.01, 20, 0.01)
+  .onFinishChange(generatePoints);
+paramFolder
+  .add(parameters, "branches", 2, 20, 1)
+  .onFinishChange(generatePoints);
+paramFolder.add(parameters, "spin", -5, 5, 1).onFinishChange(generatePoints);
+paramFolder
+  .add(parameters, "randomness", 0, 2, 0.01)
+  .onFinishChange(generatePoints);
+paramFolder
+  .add(parameters, "randomnessPower", 1, 10, 1)
+  .onFinishChange(generatePoints);
+paramFolder.addColor(parameters, "insideColor").onFinishChange(generatePoints);
+paramFolder.addColor(parameters, "outsideColor").onFinishChange(generatePoints);
 
 window.addEventListener(
   "pointermove",
@@ -141,6 +228,11 @@ let render = () => {
   state.currAudio += analysis + clock.getDelta() * 0.5;
   state.audio = 0.2 * state.currAudio + 0.8 * state.audio;
 
+  if (analysis) {
+    indexPosition++;
+    addToPosition(analysis);
+    generatePoints();
+  }
   controls.update();
   renderer.render(scene, camera);
 };
